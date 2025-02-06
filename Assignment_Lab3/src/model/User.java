78ffc32475ac828aef6f3e32158a092ac3aa5099
Nirja_iiss <nirjapatel01@github.com>
2025-02-06 17:34:45 -0500

    Added java swing lab 3 Assignment

---
+++ Assignment_Lab3/src/model/User.java
@@ -0,0 +1,83 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author arkpatel
+ */
+public class User {
+                private String fname;
+                private String lname;
+                private String gender;
+                private int age;
+                private String phone;
+                private String email;
+                private String hobbies;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(String hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    @Override
+    public String toString() {
+        return "Welcome " + fname + " " + lname + "\nGender : " + gender + "\nAge : " + age + "\nPhone : " + phone + "\nEmail : " + email + "\nHobbies : " + hobbies ;
+    }
+                
+                
+    
+}
